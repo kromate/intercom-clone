067027071f712abe9b1b4e314e55514fc8b9a96b
kromate <akpananthony33@gmail.com>
2022-04-23 14:48:20 +0100

    --- Auto-Git Commit ---

--- intercom.js
+++ intercom.js
@@ -1,20 +1,20 @@
-const initButton = () => {
+const initButton = ({ color, bgColor, scriptURL, openIcon, closeIcon}) => {
   
-loaded()
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwNddRBf6fJHnual0bmKRm6ExEHf6520FRv_2t66FO28tUBj4AiZlkr0lk2-p3iDAmf8g/exec';
-const color = 'rgb(55, 131, 55)';
-const bgColor = 'darkBlue';
-const openIcon = `<rect x="0.900146" y="4.5" width="22.2" height="15" rx="1.5" stroke="currentColor" stroke-width="1.8"/>
+
+ scriptURL =  'https://script.google.com/macros/s/AKfycbwNddRBf6fJHnual0bmKRm6ExEHf6520FRv_2t66FO28tUBj4AiZlkr0lk2-p3iDAmf8g/exec';
+ color = 'rgb(55, 131, 55)';
+ bgColor = bgColor || 'darkBlue';
+ openIcon = `<rect x="0.900146" y="4.5" width="22.2" height="15" rx="1.5" stroke="currentColor" stroke-width="1.8"/>
 <path d="M22.5 5.25L12.8719 12.1272C12.3503 12.4998 11.6497 12.4998 11.1281 12.1272L1.5 5.25" stroke="currentColor" stroke-width="1.5"/>`
-const closeIcon = `
+ closeIcon = `
 <path d="M19.9201 8.94995L13.4001 15.47C12.6301 16.24 11.3701 16.24 10.6001 15.47L4.08008 8.94995" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>å
 `
-// const color = '${bgColor}';
+loaded()
 
 
 
-functionality = function () {
+ function functionality () {
     document.querySelector('.closebtn').addEventListener('click', () => {
           document.getElementById('c123').innerHTML = openIcon
           document.getElementById('box123').style.display = 'none'
@@ -76,7 +76,7 @@ functionality = function () {
 
 }
 
-loaded = function () {
+ function loaded () {
   let head = document.querySelector('head')
   let body = document.querySelector('body')
   let style = document.createElement('style')
